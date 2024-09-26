a150cc2084f86019547d61208cc1313550c577ec
Sheldon <55363444+DevShel@users.noreply.github.com>
2024-09-26 16:59:41 -0400

    I updated the comments with more detail, because I made my first leetcode post and used this code

--- greedy/jump_game_2.py
+++ greedy/jump_game_2.py
@@ -8,19 +8,25 @@ class Solution:
         num_len = len(nums)
         if num_len == 1 or num_len == 2: return num_len - 1
         
+        # Use a dp array to track our global minimum number of jumps, 
+        # dp[i] is the minimum number of jumps required to reach nums[i].
         dp = [sys.maxsize] * num_len
         dp[0] = 0
 
+        # Calculate our farthest possible jump from this index
+        # We reset the farthest jump to within bounds, if it is longer than len(nums)
         farthest_jump = num_len - 1 if nums[0] >= num_len else nums[0]
         # Populate initially reachable areas
         for i in range(1, farthest_jump + 1):
             dp[i] = 1
 
-        # Core recursive step: what is the minimum number of jumps needed to get to the current element
+        # Go through the entire nums array
+        # Question: For each e, using our current step, 
+        # would we be able to reach 'e' sooner than what 
+        # is stored in our dp[] array? If so, update the dp array.
         for i in range(1, num_len):
-            if (dp[i] != sys.maxsize): # If we are able to reach this element through our jumps
-                farthest_jump = num_len - 1 if i + nums[i] >= num_len else i + nums[i] # reset farthest jump if out of bounds 
-                for j in range (i + 1, farthest_jump + 1): # For all currently reachable elements
-                    dp[j] = min(dp[j], dp[i] + 1) # Min the curr jump + curr distance to the curr min 
+            farthest_jump = num_len - 1 if i + nums[i] >= num_len else i + nums[i] # reset farthest jump if out of bounds 
+            for j in range (i + 1, farthest_jump + 1): # For all currently reachable elements
+                dp[j] = min(dp[j], dp[i] + 1) # Min the curr jump + curr distance to the curr min 
 
         return dp[num_len - 1]
